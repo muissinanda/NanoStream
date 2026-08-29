814dc427b48ee334004dd079d9342c3d13c1ae83
Muis Sinanda <muis.sinanda@gmail.com>
2026-08-29 15:10:23 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -85,10 +85,9 @@ def start_ffmpeg(path, source_url):
         "-timeout", "10000000",
         "-user_agent", "IPTVSmartersPro",
         "-i", source_url,
-        "-map", "0:v:0?", "-map", "0:a:0?",
         "-c:v", "copy", "-c:a", "copy",
         "-max_muxing_queue_size", "1024",
-        "-f", "rtsp", "-rtsp_transport", "tcp", f"rtsp://127.0.0.1:8554/{path}"
+        "-f", "mpegts", f"srt://127.0.0.1:8890?streamid=publish:{path}"
     ]
     
     # Menyimpan log untuk proses ini (agar mudah di debug jika gagal)
